da185cfd718b114cb911aee40a01e06c14d1becb
인연준 <48353701+YeonJun-IN@users.noreply.github.com>
2019-06-19 01:28:35 +0900

    Add files via upload

---
+++ lda.R
@@ -0,0 +1,73 @@
+rm(list=ls())
+library(dplyr)
+library(topicmodels)
+library(tidytext)
+library(ggplot2)
+
+setwd("~/Desktop/연준/프로젝트/진행중/연구실_산공/text mining")
+data <- read.csv("tdm.csv", stringsAsFactors = F, row.names = 1)
+data <- readr::read_csv("tdm.csv")
+
+data1 <- t(data)
+rownames(data1) <- NULL
+
+dim(data)
+
+a <- apply(data1,1,sum)
+data2 <- data1[-which(a == 0),]
+rm(data1)
+
+set.seed(12345) 
+sampling <- sample(1:as.integer(nrow(data2)*0.8),replace = FALSE)
+train_data <- data2[sampling,]
+test_data <- data2[-sampling,]
+
+
+perplexity_df <- data.frame(train=numeric(), test=numeric())
+topics <- c(2:30)
+burnin = 100
+iter = 1000
+keep = 50
+
+set.seed(12345)
+for (i in topics){
+  
+  fitted <- LDA(train_data, k = i, method = "Gibbs",
+                control = list(burnin = burnin, iter = iter, keep = keep) )
+  perplexity_df[i,1] <- perplexity(fitted, newdata = train_data)
+  perplexity_df[i,2]  <- perplexity(fitted, newdata = test_data) 
+}
+
+#perplexity 시각화
+g <- ggplot(data=perplexity_df, aes(x= as.numeric(row.names(perplexity_df)))) + labs(y="Perplexity",x="Number of topics") + ggtitle("Perplexity of hold out  and training data")
+
+g <- g + geom_line(aes(y=test), colour="red")
+g <- g + geom_line(aes(y=train), colour="green")
+g
+
+
+#정한 topic 개수로 토픽모델ㄹ
+lda_1 <- LDA(data2, k=15, seed=12345)
+topics <- tidy(lda_1,matrix='beta')
+top_terms <- topics %>% group_by(topic) %>% top_n(20, beta) %>% ungroup() %>% arrange(topic, -beta)
+
+
+#시각화
+top_terms %>% filter(topic == 1 | topic == 2 | topic == 3 | topic == 4 | topic == 5) %>% 
+  mutate(term=reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill=factor(topic))) +
+  geom_col(show.legend=FALSE) +
+  facet_wrap(~ topic, scales="free") +
+  coord_flip() +
+  theme(axis.text.y=element_text(family=""))
+
+top_terms %>% 
+  mutate(term=reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill=factor(topic))) +
+  geom_col(show.legend=FALSE) +
+  facet_wrap(~ topic, scales="free") +
+  coord_flip() +
+  theme(axis.text.y=element_text(family=""))
+
+
+lda_result_1 <- lda_k_seed_topn(5,123,10)
